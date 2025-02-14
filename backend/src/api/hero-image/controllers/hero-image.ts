1882a7754d610e8dc9fd00cfcf5c008ff1f893fc
tejas <tejasbm5878@gmail.com>
2025-02-14 12:47:21 +0530

    deleted backend

--- backend/src/api/hero-image/controllers/hero-image.ts
+++
@@ -1,7 +0,0 @@
-/**
- * hero-image controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::hero-image.hero-image');
--- backend/src/api/services-card/routes/services-card.ts
+++
@@ -1,7 +0,0 @@
-/**
- * services-card router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::services-card.services-card');
